e00279375fa16700d6cb32f8beb60ef74d28a43f
fpe4 <fpe4@kth.se>
2021-01-18 16:18:04 +0100

    exp working

--- test/recursive.ex
+++ test/recursive.ex
@@ -1,9 +1,10 @@
 defmodule Recursive do
+    
     def product(m, n) do
         if m == 0 do
             0
         else
-        n + (m-1)*n 
+            n + product(m-1, n)
         end
     end
 
@@ -12,8 +13,7 @@ defmodule Recursive do
             0 ->
                 0
             _ -> 
-                n + (m-1)*n 
-
+                n + product_case(m-1, n)
         end
     end
 
@@ -22,8 +22,21 @@ defmodule Recursive do
             m == 0 -> 
                 0
             true ->
-                n + (m-1)*n 
+                n + product_cond(m-1, n)
         end
     end
 
+    def product_clauses(0, _) do 0 end
+    def product_clauses(m, n) do
+        product_clauses(m-1, n) + n
+    end
+
+    def exp(x, n) do
+        case n do
+            0 ->
+                1
+            _ ->
+                product(x, exp(x, n-1))
+        end
+    end
 end
