1193343f0fa99deb41f778d176c5e6da019846de
fpe4 <fpe4@kth.se>
2021-02-11 10:57:43 +0100

    Update high.ex

--- test/high.ex
+++ test/high.ex
@@ -2,6 +2,35 @@ defmodule High do
 
     def map([], _) do [] end    
     def map([h | t], op) do [op.(h) | map(t, op)] end   
-    
-     
+
+    def filter([], _) do [] end
+    def filter([h | t], op) do 
+        if op.(h) do
+            [h | filter(t, op)]
+        else
+            filter(t, op)
+         end
+    end
+
+    def infinity() do
+        fn () -> infinity(0) end
+    end
+
+    def infinity(n) do
+        [n | fn() -> infinity(n+1) end]
+    end
+
+    def reduce({:range, from, to}, acc, fun) do
+        if from <= to do
+            reduce({:range, from + 1, to}, fun.(from,acc), fun)
+        else 
+            acc
+        end
+    end
+
+    def sum(range) do
+        reduce(range, 0, fn(x,acc) -> x + acc end)
+    end
+
+
 end
